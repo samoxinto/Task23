6035b75445deedc9d87b3bf1c1e691d87067ef8e
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-18 13:40:17 +0300

    Add project files.

---
+++ Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine(Math.Pow(i, 3));
+
+            Console.ReadLine();
+        }
+    }
+}
